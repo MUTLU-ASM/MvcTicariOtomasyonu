53cb8d3b47d315f93b8908d5806fb97746cf1987
Asim Mutlu <asimmutlu6134@gmail.com>
2023-11-11 09:34:55 +0300

    Yapilacaklar listesi tasarimi ve veritabanindan veri cekme ve Urunler listesinde ki resimleri buyultme islemi yapıildi.

---
+++ MvcTicariOtomasyonu/Controllers/YapilacakController.cs
@@ -0,0 +1,25 @@
+﻿using MvcTicariOtomasyonu.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTicariOtomasyonu.Controllers
+{
+    public class YapilacakController : Controller
+    {
+        // GET: Yapilacak
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            ViewBag.dgCari = db.currents.Count().ToString();
+            ViewBag.dgUrun = db.Uruns.Count().ToString();
+            ViewBag.dgKateg = db.Kategoris.Count().ToString();
+            ViewBag.dgCariSehir = (from x in db.currents select x.sehir).Distinct().Count().ToString();
+            var degerler = db.Yapilacaks.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ MvcTicariOtomasyonu/Models/Siniflar/Yapilacak.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyonu.Models.Siniflar
+{
+    public class Yapilacak
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(100)]
+        public string baslik { get; set; }
+
+        [Column(TypeName = "bit")]
+        public bool durum { get; set; }
+
+    }
+}
