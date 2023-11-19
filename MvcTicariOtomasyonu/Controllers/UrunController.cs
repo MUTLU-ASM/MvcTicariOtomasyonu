371a2f8c5428639f4deb486d5b929415b8efc496
Asim Mutlu <asimmutlu6134@gmail.com>
2023-11-19 19:47:05 +0300

    urun satis islemleri yapildi.

--- MvcTicariOtomasyonu/Controllers/UrunController.cs
+++ MvcTicariOtomasyonu/Controllers/UrunController.cs
@@ -13,9 +13,9 @@ namespace MvcTicariOtomasyonu.Controllers
     {
         // GET: Urun
         Context db = new Context();
-        public ActionResult Index(string p,int sayfa=1)
+        public ActionResult Index(string p, int sayfa = 1)
         {
-            var degerler = db.Uruns.Where(x=>x.ad.Contains(p)|| p==null).ToList().ToPagedList(sayfa,5);
+            var degerler = db.Uruns.Where(x => x.ad.Contains(p) || p == null).ToList().ToPagedList(sayfa, 5);
             return View(degerler);
         }
         [HttpGet]
@@ -48,11 +48,11 @@ namespace MvcTicariOtomasyonu.Controllers
         public ActionResult UrunGetir(int id)
         {
             List<SelectListItem> dgr = (from x in db.Kategoris.ToList()
-                                          select new SelectListItem
-                                          {
-                                              Text = x.ad,
-                                              Value = x.id.ToString()
-                                          }
+                                        select new SelectListItem
+                                        {
+                                            Text = x.ad,
+                                            Value = x.id.ToString()
+                                        }
                                ).ToList();
             ViewBag.dgr = dgr;
             var deger = db.Uruns.Find(id);
@@ -75,8 +75,32 @@ namespace MvcTicariOtomasyonu.Controllers
         }
         public ActionResult UrunListesi()
         {
-            var degerler=db.Uruns.ToList();
+            var degerler = db.Uruns.ToList();
             return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult SatisYap(int id)
+        {
+            List<SelectListItem> dgrPers = (from x in db.Personels.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = x.ad + " " + x.soyad,
+                                                Value = x.id.ToString()
+                                            }
+).ToList();
+            ViewBag.dgrPers = dgrPers;
+            var deger = db.Uruns.Find(id);
+            ViewBag.dgrID=deger.id;
+            ViewBag.dgrFiyat=deger.satisFiyat;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SatisYap(SatisHareket s)
+        {
+            s.tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.SatisHarekets.Add(s);
+            db.SaveChanges();
+            return RedirectToAction("Index","Satis");
         }
     }
 }
