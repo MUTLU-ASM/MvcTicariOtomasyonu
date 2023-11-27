5fc980fe0833ec0fdd4c139811d572f6af703826
Asim Mutlu <asimmutlu6134@gmail.com>
2023-11-27 14:15:26 +0300

    Yeni mesaj kisminda veri gonderimi ve alimi halledildi.

--- MvcTicariOtomasyonu/Controllers/CariPanelController.cs
+++ MvcTicariOtomasyonu/Controllers/CariPanelController.cs
@@ -15,14 +15,14 @@ namespace MvcTicariOtomasyonu.Controllers
         public ActionResult Index()
         {
             var cariMail = (string)Session["mail"];
-            var degerler=db.currents.FirstOrDefault(x=>x.mail==cariMail);
+            var degerler = db.currents.FirstOrDefault(x => x.mail == cariMail);
             ViewBag.m = cariMail;
             return View(degerler);
-        }  
+        }
         public ActionResult Siparislerim()
         {
             var cariMail = (string)Session["mail"];
-            var id=db.currents.Where(x=>x.mail==cariMail.ToString()).Select(y=>y.id).FirstOrDefault();
+            var id = db.currents.Where(x => x.mail == cariMail.ToString()).Select(y => y.id).FirstOrDefault();
             var degerler = db.SatisHarekets.Where(x => x.id == id).ToList();
             return View(degerler);
         }
@@ -30,8 +30,8 @@ namespace MvcTicariOtomasyonu.Controllers
         public ActionResult GelenMesajlar()
         {
             var cariMail = (string)Session["mail"];
-            var degerler = db.Mesajlars.Where(x=>x.alici==cariMail).ToList();
-            var gelenSayisi=db.Mesajlars.Count(x=>x.alici==cariMail).ToString();
+            var degerler = db.Mesajlars.Where(x => x.alici == cariMail).OrderByDescending(x=>x.id).ToList();
+            var gelenSayisi = db.Mesajlars.Count(x => x.alici == cariMail).ToString();
             ViewBag.gelenMesaj = gelenSayisi;
             var gidenSayisi = db.Mesajlars.Count(x => x.gonderici == cariMail).ToString();
             ViewBag.gidenMesaj = gidenSayisi;
@@ -40,14 +40,25 @@ namespace MvcTicariOtomasyonu.Controllers
         public ActionResult GidenMesajlar()
         {
             var cariMail = (string)Session["mail"];
-            var degerler = db.Mesajlars.Where(x => x.gonderici == cariMail).ToList();
+            var degerler = db.Mesajlars.Where(x => x.alici == cariMail).OrderByDescending(x => x.id).ToList();
             var gidenSayisi = db.Mesajlars.Count(x => x.gonderici == cariMail).ToString();
             ViewBag.gidenMesaj = gidenSayisi;
             var gelenSayisi = db.Mesajlars.Count(x => x.alici == cariMail).ToString();
             ViewBag.gelenMesaj = gelenSayisi;
             return View(degerler);
         }
-        public ActionResult MesajDetay()
+        public ActionResult MesajDetay(int id)
+        {
+            var degerler = db.Mesajlars.Where(x => x.id == id).ToList();
+            var cariMail = (string)Session["mail"];
+            var gidenSayisi = db.Mesajlars.Count(x => x.gonderici == cariMail).ToString();
+            ViewBag.gidenMesaj = gidenSayisi;
+            var gelenSayisi = db.Mesajlars.Count(x => x.alici == cariMail).ToString();
+            ViewBag.gelenMesaj = gelenSayisi;
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniMesaj()
         {
             var cariMail = (string)Session["mail"];
             var gidenSayisi = db.Mesajlars.Count(x => x.gonderici == cariMail).ToString();
@@ -56,15 +67,15 @@ namespace MvcTicariOtomasyonu.Controllers
             ViewBag.gelenMesaj = gelenSayisi;
             return View();
         }
-        [HttpGet]
-        public ActionResult YeniMesaj()
+        [HttpPost]
+        public ActionResult YeniMesaj(Mesajlar m)
         {
+            var cariMail = (string)Session["mail"];
+            m.tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            m.gonderici = cariMail;
+            db.Mesajlars.Add(m);
+            db.SaveChanges();
             return View();
         }
-        //[HttpPost]
-        //public ActionResult YeniMesaj()
-        //{
-        //    return View();
-        //}
     }
 }
